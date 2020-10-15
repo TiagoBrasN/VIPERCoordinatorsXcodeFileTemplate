0d706a9f9b4ae3ff7a4ff118606ecbdd99a9376f
Tiago Bras <tibr@nodesagency.com>
2020-10-15 16:15:41 +0100

    Implements Basic Interactor
    This interactor doesn't have an output delegate defined.
    
    The rational behind this is to promote an API where the caller
    passes a completion handler or the callee returns some kind of
    Promise/Future instead of using delegates.
    
    Using delegates adds unnecessary state management to the presenter
    since it's hard to tell which output corresponds to which input.

---
+++ VIPER Coordinators/Basic Interactor.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import Foundation
+
+class ___VARIABLE_sceneName___Interactor {
+    // MARK: - Properties
+
+    // MARK: - Init
+    init() {
+
+    }
+}
+
+// MARK: - Business Logic -
+
+// PRESENTER -> INTERACTOR
+extension ___VARIABLE_sceneName___Interactor: ___VARIABLE_sceneName___InteractorInput {
+    
+}
